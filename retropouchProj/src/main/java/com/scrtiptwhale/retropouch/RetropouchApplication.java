bcb9637d87d4583f26866a378f66b9e820a30042
hoon <hoon111465@gmail.com>
2020-06-10 15:42:54 +0900

    Web Project for Spring Boot

---
+++ retropouchProj/src/main/java/com/scrtiptwhale/retropouch/RetropouchApplication.java
@@ -0,0 +1,13 @@
+package com.scrtiptwhale.retropouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetropouchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetropouchApplication.class, args);
+    }
+
+}
---
+++ retropouchProj/src/test/java/com/scrtiptwhale/retropouch/RetropouchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrtiptwhale.retropouch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetropouchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
